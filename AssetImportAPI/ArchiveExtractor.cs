ddde561db25daa0becc43f9df86df32157199af1
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-09-26 16:02:58 -0500

    Initial commit

---
+++ AssetImportAPI/ArchiveExtractor.cs
@@ -0,0 +1,65 @@
+﻿using BaseX;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AssetImportAPI
+{
+    public class ArchiveExtractor
+    {
+        /// <summary>
+        /// <para>
+        /// Accepts an array of archives as input and for each:
+        /// </para>
+        /// <para>
+        /// 1) Generates a unique MD5 hash, using the archive itself as a seed.
+        /// </para>
+        /// <para>
+        /// 2) Creates and names a directory inside MyAssetImporter.cachePath using a string representation of hash
+        /// </para>
+        /// <para>
+        /// 3) Directs the "Unpack" method in this file to extract the contents of the particular archive into this folder
+        /// </para>
+        /// </summary>
+        /// <param name="files">The input archives to process.</param>
+        public static string[] DecomposeArchives(string[] files)
+        {
+            var fileToHash = files.ToDictionary(file => file, Utils.GenerateMD5);
+            HashSet<string> dirsToImport = new();
+            HashSet<string> archivesToDecompress = new();
+            foreach (var element in fileToHash)
+            {
+                var dir = Path.Combine(MyAssetImporter.cachePath, element.Value);
+                if (!Directory.Exists(dir))
+                    archivesToDecompress.Add(element.Key);
+                else
+                    dirsToImport.Add(dir);
+            }
+            foreach (var package in archivesToDecompress)
+            {
+                var packageName = Path.GetFileNameWithoutExtension(package);
+                if (Utils.ContainsUnicodeCharacter(packageName))
+                {
+                    UniLog.Error($"Imported archive {packageName} cannot have unicode characters in its file name.");
+                    continue;
+                }
+                var extractedPath = Path.Combine(MyAssetImporter.cachePath, fileToHash[package]);
+                Directory.CreateDirectory(extractedPath);
+                Unpack(package, extractedPath);
+                dirsToImport.Add(extractedPath);
+            }
+            return dirsToImport.ToArray();
+        }
+
+        /// <summary>
+        /// Extracts the archive into a unique folder. This method must be implemented!
+        /// </summary>
+        /// <param name="package">The absolute path of the archive to extract.</param>
+        /// <param name="outputPath">The absolute path of the unique MD5-hashed directory to extract the archive to.</param>
+        private static void Unpack(string package, string outputPath)
+        {
+            throw new NotImplementedException("Package decompression method needs to be defined!");
+        }
+    }
+}
---
+++ AssetImportAPI/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace AssetImportAPI
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Utility method to identify unicode characters in a given string.
+        /// </summary>
+        /// <param name="input">The string to test against</param>
+        /// <returns>A boolean indicating if the string contains a unicode character.</returns>
+        public static bool ContainsUnicodeCharacter(string input)
+        {
+            const int MaxAnsiCode = 255;
+            return input.Any(c => c > MaxAnsiCode);
+        }
+
+        /// <summary>
+        /// Utility method to generate a MD5 hash for a given filepath.
+        /// Credit to delta for this method https://github.com/XDelta/
+        /// </summary>
+        /// <param name="filepath">The filepath to generate an MD5 for.</param>
+        /// <returns>A MD5 representation of the string.</returns>
+        public static string GenerateMD5(string filepath)
+        {
+            using var hasher = MD5.Create();
+            using var stream = File.OpenRead(filepath);
+            var hash = hasher.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "");
+        }
+    }
+}
